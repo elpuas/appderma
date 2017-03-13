578f14c0b8b885871916f78d967000dd67a85ede
alfredo navas <elpuas@gmail.com>
2017-03-13 19:56:31 -0400

    working validation

--- assets/js/app-scripts.js
+++ assets/js/app-scripts.js
@@ -4,16 +4,25 @@ jQuery(document).ready(function () {
         error.insertBefore(element);
           },
         rules: {
+          'wpv-condicion-de-la-piel[]': {
+              required: true,
+              maxlength: 3
+          },
             'wpv-lesion-o-patologia[]': {
                 required: true,
                 maxlength: 10
             }
         },
         messages: {
+            'wpv-condicion-de-la-piel[]': {
+                required: "Debes Seleccionar por lo menos una",
+                maxlength: "No Puedes seleccionar mas de {3} cajas"
+            },
             'wpv-lesion-o-patologia[]': {
                 required: "Debes Seleccionar por lo menos una",
                 maxlength: "No Puedes seleccionar mas de {10} cajas"
             }
+
         },
     });
 });
