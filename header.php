db68e3fb828b8203d1cc1ec5edb015c2b21d8fe1
elpuas <elpuas@gmail.com>
2017-03-19 19:07:48 -0500

    Theme ready for review

--- header.php
+++ header.php
@@ -12,6 +12,7 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
+<meta name="apple-itunes-app" content="app-id=1212148145">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
